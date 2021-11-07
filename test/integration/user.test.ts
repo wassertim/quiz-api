a08ff94e94094e41d181649e285469c343f66763
wassertim <tim.tkachenko@idealo.de>
2021-11-07 18:00:25 +0100

    add login integration test

--- test/integration/user.test.ts
+++ test/integration/user.test.ts
@@ -3,6 +3,10 @@ import request from "supertest";
 import {constants} from "http2";
 import {initDatabase} from "./util/init.db";
 
+async function registerUser(user: { password: string; login: string }) {
+  await request(app).post("/users/register").send(user);
+}
+
 describe("Register User API", () => {
   initDatabase();
   test("Should register a user", async () => {
@@ -12,11 +16,12 @@ describe("Register User API", () => {
     expect(response.statusCode).toBe(constants.HTTP_STATUS_CREATED);
   });
   test("Should return an error when user exists", async () => {
-    await request(app).post("/users/register").send({login: "laura", password: "mypassword"});
+    const user = {login: "laura", password: "mypassword"};
+    await registerUser(user);
 
     const response = await request(app)
         .post("/users/register")
-        .send({login: "laura", password: "mypassword"});
+        .send(user);
 
     expect(response.statusCode).toBe(constants.HTTP_STATUS_BAD_REQUEST);
     expect(response.text).toBe("User with login laura already exists");
@@ -40,6 +45,18 @@ describe("Register User API without DB", () => {
     expect(response.text).not.toBeFalsy();
   });
 });
-// describe("Login User API", () => {
-//   test("Should login user")
-// });
+describe("Login User API", () => {
+  initDatabase();
+  const user = {login: "laura", password: "mypassword"};
+  beforeEach (() => {
+    return registerUser(user);
+  });
+  test("Should login user", async () => {
+    const response = await request(app)
+        .post("/users/login")
+        .send(user);
+
+    expect(response.statusCode).toBe(constants.HTTP_STATUS_OK);
+    expect(response.text).toBe("Basic bGF1cmE6bXlwYXNzd29yZA==");
+  });
+});
