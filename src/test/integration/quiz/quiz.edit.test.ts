7afa3136200f4ff80f0e0dc6ef786341b4a79d1e
wassertim <tim.tkachenko@idealo.de>
2021-11-10 10:33:55 +0100

    refactor quiz integration tests

--- src/test/integration/quiz/quiz.edit.test.ts
+++ src/test/integration/quiz/quiz.edit.test.ts
@@ -1,73 +1,11 @@
-import { initDatabase } from "./util/init.db";
-import { createQuiz, login, registerUser } from "./util/api";
-import request from "supertest";
-import { app } from "../../app";
-import { constants } from "http2";
 import { Operation } from "express-openapi-validate/dist/OpenApiDocument";
-import { openapi } from "./util/openapi";
+import { createQuiz, login, registerUser } from "../util/api";
+import { initDatabase } from "../util/init.db";
+import request from "supertest";
+import { app } from "../../../app";
+import { openapi } from "../util/openapi";
+import { constants } from "http2";
 
-describe("Create Quiz API", () => {
-    initDatabase();
-    const user = { login: "laura", password: "mypassword" };
-    beforeEach(() => {
-        return registerUser(user);
-    });
-    test("Should create a quiz", async () => {
-        const [method, path] = ["post" as Operation, "/profiles/{login}/quizzes"];
-        const token = await login(user);
-
-        const response = await request(app)
-            [method](path.replace("{login}", user.login))
-            .set({ Authorization: token })
-            .send({
-                questions: [
-                    {
-                        questionText: "what is the answer to life the universe and everything",
-                        questionScore: 5,
-                        answers: [
-                            {
-                                text: "42",
-                                isCorrect: true,
-                            },
-                            {
-                                text: "41",
-                                isCorrect: false,
-                            },
-                        ],
-                    },
-                ],
-            });
-
-        expect(openapi.validateResponse(method, path)(response)).toBeUndefined();
-        expect(response.statusCode).toBe(constants.HTTP_STATUS_CREATED);
-        expect(response.body.createdBy).toBe(user.login);
-    });
-    test("Should give an error when invalid quiz", async () => {
-        const [method, path] = ["post" as Operation, "/profiles/{login}/quizzes"];
-        const token = await login(user);
-
-        const response = await request(app)
-            [method](path.replace("{login}", user.login))
-            .set({ Authorization: token })
-            .send({
-                questions: [
-                    {
-                        questionScore: 5,
-                        answers: [
-                            {
-                                text: "42",
-                                isCorrect: true,
-                            },
-                        ],
-                    },
-                ],
-            });
-
-        expect(openapi.validateResponse(method, path)(response)).toBeUndefined();
-        expect(response.statusCode).toBe(constants.HTTP_STATUS_BAD_REQUEST);
-        expect(response.text).toBe('"questions[0].questionText" is required');
-    });
-});
 describe("Edit Quiz API", () => {
     initDatabase();
     const user = { login: "laura", password: "mypassword" };
