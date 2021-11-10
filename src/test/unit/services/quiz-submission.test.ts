ca5461e4797fe89ab2006d72ba439a7848ba14e5
wassertim <tim.tkachenko@idealo.de>
2021-11-10 19:20:12 +0100

    fix tests

--- src/test/unit/services/quiz-submission.test.ts
+++ src/test/unit/services/quiz-submission.test.ts
@@ -1,23 +1,18 @@
-import { err, ok } from "neverthrow";
+import { ok } from "neverthrow";
 import { QuizAttempts } from "../../../db";
-import { createQuiz } from "../../../services/quiz.service";
-import { QuizErrors } from "../../../types/errors";
 import { getMockedCollection } from "./util/mock";
-import { validate } from "../../../validators/quiz.validator";
-import { mocked } from "ts-jest/utils";
 import { createQuizAttempt } from "../../../services/quiz-attempt.service";
 
 jest.mock("../../../db");
 jest.mock("../../../validators/quiz.validator");
 
-describe("Quiz Service Create Quiz", () => {
-    test("Should return ok with quiz", async () => {
+describe("Quiz Submission Service", () => {
+    test("Should return ok with quiz submission", async () => {
         const quizSubmission = {
-            quizId: "618c05f4236ff44323b8dd9e"
+            quizId: "618c05f4236ff44323b8dd9e",
         };
         const mockUserCollection = getMockedCollection(QuizAttempts);
-        mockUserCollection.insertOne = jest.fn().mockImplementation(() => ({ ...quizSubmission, insertedId: 42 }));
-        mocked(validate).mockReturnValue(ok(quizSubmission));
+        mockUserCollection.insertOne = jest.fn().mockImplementation(() => ({ ...quizSubmission, insertedId: 42 }));        
 
         const result = await createQuizAttempt(quizSubmission);
 
