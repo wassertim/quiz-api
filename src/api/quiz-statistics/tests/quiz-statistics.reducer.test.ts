842029cacfc94e5259168168c01acf6a5462eaae
wassertim <tim.tkachenko@idealo.de>
2021-11-11 20:56:15 +0100

    add test for statistics

--- src/api/quiz-statistics/tests/quiz-statistics.reducer.test.ts
+++ src/api/quiz-statistics/tests/quiz-statistics.reducer.test.ts
@@ -53,6 +53,34 @@ describe("Statistics", () => {
     test("should map", () => {
         const stat = buildStatistics(quiz, [qs]);
 
-        expect(stat).toBeDefined(); // TODO: Make proper assertion
+        expect(stat).toStrictEqual({
+            quizId: "asdf",
+            submissionsStat: [
+                {
+                    correctAnswersCount: 1,
+                    id: undefined,
+                    questionsAndAnswers: [
+                        {
+                            completed: true,
+                            isCorrect: true,
+                            questionIndex: 0,
+                            questionText: "Yes?",
+                            score: 5,
+                        },
+                        {
+                            completed: true,
+                            isCorrect: false,
+                            questionIndex: 1,
+                            questionText: "Question 2",
+                            score: 0,
+                        },
+                    ],
+                    scoreTotal: 5,
+                    totalCompletedQuestions: 2,
+                    totalQuestionsInQuiz: 2,
+                },
+            ],
+            totalSubmissions: 1,
+        });
     });
 });
