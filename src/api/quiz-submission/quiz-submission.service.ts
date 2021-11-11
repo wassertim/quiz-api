4d7d145a980103abbb8959591c7cd65bc0c59005
wassertim <tim.tkachenko@idealo.de>
2021-11-11 22:03:32 +0100

    finis

--- src/api/quiz-submission/quiz-submission.service.ts
+++ src/api/quiz-submission/quiz-submission.service.ts
@@ -5,7 +5,7 @@ import { ApiError } from "../../errors/errors";
 
 export async function insertQuizSubmission(quiz: QuizSubmission) {        
     try {        
-        const result = await QuizSubmissions().insertOne(quiz);
+        const result = await QuizSubmissions().insertOne({...quiz});
 
         return ok({ ...quiz, id: result.insertedId + "" } as QuizSubmission);
     } catch (e) {
