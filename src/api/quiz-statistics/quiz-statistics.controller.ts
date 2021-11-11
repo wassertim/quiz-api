8eb62ca729f53ed99cae0d5064f00ff83777d199
wassertim <tim.tkachenko@idealo.de>
2021-11-11 19:11:05 +0100

    wip

--- src/api/quiz-statistics/quiz-statistics.controller.ts
+++ src/api/quiz-statistics/quiz-statistics.controller.ts
@@ -2,6 +2,7 @@ import { Request, Response } from "express";
 import { findQuizStatistics } from "./quiz-statistics.service";
 
 export async function getQuizStatistics(req: Request, res: Response<unknown | string>) {
+    // TODO: Make proper error handling
     const stat = await findQuizStatistics(req.params.quizId);
     res.send(stat);
 }
